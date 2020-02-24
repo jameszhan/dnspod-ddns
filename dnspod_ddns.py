7ef5a2af8cc25720f5cc535485ca713772a89f2f
James Zhan <zhiqiangzhan@gmail.com>
2020-02-24 23:21:49 +0800

    fix it

--- dnspod_ddns.py
+++ dnspod_ddns.py
@@ -21,27 +21,30 @@ else:
     login_token = os.environ['DNSPOD_LOGIN_TOKEN']
 
 def synchorize_myip():
-    dns_record_id = get_dns_record_id()
+    dns_record_id, dns_record_value = get_dns_record_id()
     if dns_record_id > 0:
         myip = get_myip()
-        r = requests.post('https://dnsapi.cn/Record.Modify', data = {
-            'format': 'json',
-            'record_id': dns_record_id,
-            'value': myip,
-            'domain': dnspod_domain,
-            'sub_domain': dnspod_sub_domain,
-            'login_token': login_token,
-            'record_type': 'A',
-            'record_line': '默认'
-        })
-        print(r.json())
+        if dns_record_value != myip:
+            r = requests.post('https://dnsapi.cn/Record.Modify', data = {
+                'format': 'json',
+                'record_id': dns_record_id,
+                'value': myip,
+                'domain': dnspod_domain,
+                'sub_domain': dnspod_sub_domain,
+                'login_token': login_token,
+                'record_type': 'A',
+                'record_line': '默认'
+            })
+            print(r.json())
+        else:
+            print("ip is {0}, not changed".format(myip))
     else:
         print('{0}.{1} not exists or login token is invalid.'.format(dnspod_sub_domain, dnspod_domain))
 
 def get_myip():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect(("ns1.dnspod.net", 6666))
-        return s.recv(16)
+        return str(s.recv(16))
 
 def get_dns_record_id():
     r = requests.post('https://dnsapi.cn/Record.List', data = {
@@ -51,7 +54,8 @@ def get_dns_record_id():
         'login_token': login_token,
         'record_type': 'A'
     })
-    return int(r.json().get('records', [{}])[0].get('id', '0'))
+    record = r.json().get('records', [{}])[0]
+    return (int(record.get('id', '0')), record.get('value'))
 
 if __name__ == '__main__':
     synchorize_myip()
