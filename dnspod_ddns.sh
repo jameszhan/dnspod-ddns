02e5c59414cfa17ed54346c856313ca04e1390fc
James Zhan <zhiqiangzhan@gmail.com>
2020-02-26 00:34:48 +0800

    add bash implementation for dnspod_ddns

--- dnspod_ddns.sh
+++ dnspod_ddns.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 set -o noglob
 
-DnspodSubDomain=$1
-DnspodDomain=$2
-DnspodLoginToken=$3
+dnspodSubDomain=$1
+dnspodDomain=$2
+dnspodLoginToken=$3
 
-if [ -z $DnspodSubDomain ]; then
-    DnspodSubDomain=$DNSPOD_SUB_DOMAIN
+if [ -z $dnspodSubDomain ]; then
+    dnspodSubDomain=$DNSPOD_SUB_DOMAIN
 fi
 
-if [ -z $DnspodDomain ]; then
-    DnspodDomain=$DNSPOD_DOMAIN
+if [ -z $dnspodDomain ]; then
+    dnspodDomain=$DNSPOD_DOMAIN
 fi
 
-if [ -z $DnspodLoginToken ]; then
-    DnspodLoginToken=$DNSPOD_LOGIN_TOKEN
+if [ -z $dnspodLoginToken ]; then
+    dnspodLoginToken=$DNSPOD_LOGIN_TOKEN
 fi
 
-PostData="login_token=$DnspodLoginToken&format=json&domain=$DnspodDomain&sub_domain=$DnspodSubDomain&record_type=A"
+postData="login_token=$dnspodLoginToken&format=json&domain=$dnspodDomain&sub_domain=$dnspodSubDomain&record_type=A"
 
-result=$(wget -qO- --no-check-certificate --content-on-error --post-data "$PostData" https://dnsapi.cn/Record.List)
+records=$(wget -qO- --no-check-certificate --content-on-error --post-data "$postData" https://dnsapi.cn/Record.List)
+recordId=$(echo $records | python -c "import sys,json; ret=json.load(sys.stdin); print(ret.get('records', [{}])[0].get('id', ''))")
+recordIP=$(echo $records | python -c "import sys,json; ret=json.load(sys.stdin); print(ret.get('records', [{}])[0].get('value', ''))")
 
-echo $result
+myIP=$(wget -qO- --no-check-certificate --content-on-error http://ns1.dnspod.net:6666)
+
+if [ $recordIP = $myIP ]; then
+    echo "IP not changed"
+else
+    updatePostData="$postData&record_id=$recordId&value=$myIP&record_line=默认"
+    echo $(wget -qO- --no-check-certificate --content-on-error --post-data "$updatePostData" https://dnsapi.cn/Record.Modify)
+fi
+
